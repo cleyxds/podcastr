3e61d2620f1ffe08ab1b3a58f755b00cc96edbf9
cleyxds <cleysonn@hotmail.com.br>
2021-05-03 20:59:29 -0300

    Slider and buttons styles

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
 import { useContext } from 'react';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
 
 import { PlayerContext } from '../../contexts/PlayerContext';
 
@@ -38,25 +41,33 @@ export function Player() {
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            { episode ? (
+              <Slider 
+                trackStyle={{ backgroundColor: '#04D361' }}
+                railStyle={{ backgroundColor: '#9F75FF' }}
+                handleStyle={{ borderColor: '#04D361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            ) }
           </div>
           <span>00:00</span>
         </div>
         
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar"/>
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior"/>
           </button>
-          <button type="button" className={styles.playButton}>
+          <button type="button" className={styles.playButton} disabled={!episode}>
             <img src="/play.svg" alt="Tocar"/>
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima"/>
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir"/>
           </button>
         </div>
